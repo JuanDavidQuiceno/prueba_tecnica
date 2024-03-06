4d4b82f46e6817deee1eda4f7e207f440e199574
JuanDavidQuiceno <quicenojuan996@gmail.com>
2024-03-06 16:26:15 -0500

    implementation detail

--- lib/src/feature/home/presentation/home/car_detailt/cat_detail_scree.dart
+++ lib/src/feature/home/presentation/home/car_detailt/cat_detail_scree.dart
@@ -1,3 +1,4 @@
+import 'package:custom_image/custom_image.dart';
 import 'package:flutter/material.dart';
 import 'package:prueba_tecnica/src/feature/home/domain/models/home_model.dart';
 
@@ -8,6 +9,118 @@ class CatDetailtScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Hero(
+          tag: '',
+          child: Text(
+            catModel.name ?? '',
+            style: Theme.of(context).textTheme.titleLarge,
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: size.height * 0.5,
+            child: CustomImage(
+              catModel.wikipediaUrl ?? '',
+              type: ImageType.network,
+              height: size.height * 0.5,
+            ),
+          ),
+          Expanded(
+            child: Scrollbar(
+              child: ListView(
+                children: [
+                  if (catModel.name != null)
+                    ListTile(
+                      leading: const Icon(Icons.pets),
+                      title: Text(
+                        catModel.name!,
+                        style: Theme.of(context).textTheme.displayMedium,
+                      ),
+                    ),
+                  if (catModel.origin != null)
+                    ListTile(
+                      leading: const Icon(Icons.location_on),
+                      title: Text(
+                        catModel.origin!,
+                        style: Theme.of(context).textTheme.displayMedium,
+                      ),
+                    ),
+                  if (catModel.temperament != null)
+                    ListTile(
+                      leading: const Icon(Icons.emoji_nature),
+                      title: Text(
+                        catModel.temperament!,
+                        style: Theme.of(context).textTheme.displayMedium,
+                      ),
+                    ),
+                  if (catModel.lifeSpan != null)
+                    ListTile(
+                      leading: const Icon(Icons.date_range),
+                      title: Text(
+                        catModel.lifeSpan!,
+                        style: Theme.of(context).textTheme.displayMedium,
+                      ),
+                    ),
+                  if (catModel.wikipediaUrl != null)
+                    ListTile(
+                      leading: const Icon(Icons.link),
+                      title: Text(
+                        catModel.wikipediaUrl!,
+                        style: Theme.of(context).textTheme.displayMedium,
+                      ),
+                    ),
+                  if (catModel.description != null)
+                    ListTile(
+                      leading: const Icon(Icons.description),
+                      title: Text(
+                        catModel.description!,
+                        style: Theme.of(context).textTheme.displayMedium,
+                      ),
+                    ),
+                  if (catModel.adaptability != null)
+                    ListTile(
+                      leading: const Icon(Icons.star),
+                      title: Text(
+                        catModel.adaptability!.toString(),
+                        style: Theme.of(context).textTheme.displayMedium,
+                      ),
+                    ),
+                  if (catModel.affectionLevel != null)
+                    ListTile(
+                      leading: const Icon(Icons.star),
+                      title: Text(
+                        catModel.affectionLevel!.toString(),
+                        style: Theme.of(context).textTheme.displayMedium,
+                      ),
+                    ),
+                  if (catModel.childFriendly != null)
+                    ListTile(
+                      leading: const Icon(Icons.star),
+                      title: Text(
+                        catModel.childFriendly!.toString(),
+                        style: Theme.of(context).textTheme.displayMedium,
+                      ),
+                    ),
+                  if (catModel.dogFriendly != null)
+                    ListTile(
+                      leading: const Icon(Icons.star),
+                      title: Text(
+                        catModel.dogFriendly!.toString(),
+                        style: Theme.of(context).textTheme.displayMedium,
+                      ),
+                    ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
